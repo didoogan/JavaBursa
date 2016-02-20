a809da68bb0405110c559ebe943ec7fefc7c66d4
Dima Timoshenko <tzd0409@gmail.com>
2016-02-21 00:08:49 +0200

    Second commit

--- Home Task 2 - JavaBasic/src/Circule.java
+++ Home Task 2 - JavaBasic/src/Circule.java
@@ -13,23 +13,23 @@ public class Circule {
 	
 	public double [][] getCircule (int a , int b, int radius) {
 		
-		double [][] circule = new double [2][radius *4 ];
+		double [][] circule = new double [2][(radius  *4)+1 ];
 		double y = 0;
 			
 
-		for (int x = 0; x < radius ; x++ ) {
+		for (int x = 0; x <= radius ; x++ ) {
 
 			y =  Math.sqrt( (radius * radius) - (x * x) ); 
 
 			circule[0][x] = x + a;
 			circule[1][x] = y + b;
 			circule[0][x + radius  ] = x + a;
-			circule[1][x + radius] = -(y + b);
+			circule[1][x + radius] = -y + b;
 
-			circule[0][x + 2*radius] = -(x + a);
+			circule[0][x + 2*radius] = -x + a;
 			circule[1][x + 2*radius] = y + b;
-			circule[0][x + 3*radius] = -(x + a);
-			circule[1][x + 3*radius] = -(y + b);
+			circule[0][x + 3*radius] = -x + a;
+			circule[1][x + 3*radius] = -y + b;
 
 
 
@@ -43,42 +43,44 @@ public class Circule {
 		boolean result = false;
 		for (int i = 0; i < firstCircule.length ; i++ ) {
 			for (int j =0; j < firstCircule[i].length ; j++ ) {
-				for (int k = 0; k < secondCircule.length ; k++ ) {
-					for (int l = 0; l < secondCircule[k].length  ;l++ ) {
+				
+				for (int l = 0; l < secondCircule[i].length  ;l++ ) {
 
-						if (firstCircule[0][j] == secondCircule[0][l] &&
-								firstCircule[1][j] == secondCircule[1][l]) {
-							System.out.printf("%f  %f;", firstCircule[0][j], firstCircule[1][j] );
-							System.out.println();
-						} else {
-							System.out.println("No coincedense");
-						}
+					if (firstCircule[0][j] == secondCircule[0][l] &&
+							firstCircule[1][j] == secondCircule[1][l]) {
+						System.out.printf("%f  %f;", firstCircule[0][j], firstCircule[1][j] );
+						System.out.println();
+					} else {
+//						System.out.println("No coincedense");
 					}
 				}
+				
 			}
 		}
 		//return result;
 	}
 	
+	public void printCircule (double[][] array) {
+		for (int j = 0; j < array[0].length; j++) {
+			System.out.println();
+			int i = 0;
+			for ( i = 0;  i < array.length; i++) {
+				System.out.printf("%f   ",  array[i][j]);
+			}
+		}
+	}
+	
 
 	public static void main(String[] args) {
-		Circule circule = new Circule(0, 0, 4);
-		Circule circule2 = new Circule(8,0, 4);
+		Circule circule = new Circule(0, 0, 10);
+		Circule circule2 = new Circule(9,0, 3);
 		double [][] array = circule.getCircule(circule.x, circule.y, circule.radius);
 		double [][] array2 = circule2.getCircule(circule2.x, circule2.y, circule2.radius);
 
-		//int count = 0;
-//		for (int j = 0; j < array2[0].length; j++) {
-//			System.out.println();
-//			int i = 0;
-//		
-//			for ( i = 0;  i < array2.length; i++) {
-//				
-//			
-//			System.out.printf("%f   ",  array2[i][j]);
-//			}
-//		}
 		circule.compareCircules( array,array2);
+//		circule.printCircule(array);
+//		System.out.println();
+//		circule.printCircule(array2);
 		
 	
 
